e6e6f351caf3e3171df9cff58e1bb4b82562239c
Jilson Correia <correia.ji@northeastern.edu>
2024-02-01 15:59:41 -0500

    table script

--- Assignment-3/script.js
+++ Assignment-3/script.js
@@ -17,70 +17,36 @@ var socialMedia = {
 
 var t = new Title("CONNECT WITH ME!");
 
-// addition on my end \/
 
-
-// for onload actions 
-window.onload = function() {
-  const col = [];
-  const checbox = document.querySelectorAll('input[type = checkbox]');
-  for( i=0 ; i < checkbox.length ; i++ ) {
-    checboxChange(i);
-  }
-}
-
-
-//for the image ction function
-function imageAction() {
-  var imgTag = document.querySelectorAll("img");
-  var dropText = document.getElementsByClassName("dropDownTextArea");
-    for(i = 0 ; i < imgTag.length ; i++ )
-    {
-      imgTag[i].onclick = function() {
-        var parentTextNode = this.parentElement;
-        var textNode = parentTextNode.parentElement.nextElementSibling;
-        if(textNode.style.display === "block")
-        {
-          textNode.style.display = "none";
-        }
-        else 
-        {
-          textNode.style.display = "block";
-        }
-      }
-    }
-}
-
-
-// for add and delete the columns
-const delete_col = document.getElementsByClassName("delete_col");
-const edit_col = document.getElementsByClassName("edit_col");
-
+let tableLength;
+// to edit and delete the columns
+const delete_col = document.getElementsByClassName("delete_col"); 
+const edit_col = document.getElementsByClassName("edit_col"); 
+// get the elements from TH
 var count_checkbox = 0;
 
-// for adding new elements 
+//to add new student on clicking Add New Student
 function new_element() {
-  var len = table.length;
   var table = document.querySelectorAll('tbody > tr');
-  var col = document.getElementsById("myTable").rows[0].cells.length;
+  var tablen = table.length;
+  var col = document.getElementById("myTable").rows[0].cells.length;
   const max = 99999;
   const min = 10000;
   var row_count = 0;
   for( i = 1 ; i <= table.length ; i++ ){
     if( i % 2 != 0 ) {
-      row_count++ ;
+      row_count++;
     }
   }
-
   var table_1 = document.getElementById("myTable");
 
-  var row_1 = table_1.insert_row();
+  var row_1 = table_1.insertRow();
   const td = [];
-  for( i = 0 ; i < 10 ; i++ ) {
-    td[i] = row_1.insert_cell(i);
+  for ( i = 0 ; i < 10 ; i++ ){
+   td[i] = row_1.insertCell(i);
   }
 
-  td[0].innerHTML = <td><input type="checkbox" onclick="checkboxChange(this)"/><br /><br /><img src="down.png" width="25px" onclick = "imageAction()"/></td>;
+  td[0].innerHTML = `<td><input type="checkbox" onclick="checkboxChange()" /><br /><br /><img onclick="imageAction(this)" src="down.png" width="25px" /></td>`; 
   td[1].innerHTML = "Student "+ row_count;
   td[2].innerHTML = "Teacher "+ row_count;
   td[3].innerHTML = "Approved";
@@ -108,29 +74,27 @@ function new_element() {
   Award Status:<br /><br /><br />
   </td>`;
   td_1.parentNode.hidden = true;
-  checkboxChange(Math.floor(len/2));
+  checkboxChange(Math.floor(tablen/2));
 }
 
-// function to edit rows
-function edit_row(row) {
-
-  prompt("Edit contents of the Row");
+// function for onload
+window.onload = function() {
+  const col = [];
+  const checkbox = document.querySelectorAll('input[type = checkbox]');
+  for(i=0; i<checkbox.length; i++) {
+    checkboxChange(i);
+  }
 }
 
-
-
-
-
-
-//Button, Row color functions pertaining to checkbox
+//checkbox change function for the buttons rows
 function checkboxChange(value) {
   const checkbox = document.querySelectorAll('input[type = checkbox]');
     checkbox[value].addEventListener("change", function() {
 
-      var submitBtn = document.getElementById('button');
-      var parentElm = this.parentElement.parentElement.lastElementChild; //Edit Col
-      var delBtn = parentElm.previousElementSibling.style.display; //Delete Col Style
-      var editBtn = parentElm.style.display;
+      var submit_button = document.getElementById('button');
+      var parent_element = this.parentElement.parentElement.lastElementChild; 
+      var del_btn = parent_element.previousElementSibling.style.display;
+      var edit_btn = parent_element.style.display;
       
       checkbox[value].checked ? count_checkbox++ : count_checkbox--;
       if(count_checkbox > 0)
@@ -138,21 +102,21 @@ function checkboxChange(value) {
         document.getElementById("button").style.backgroundColor = "orange";
         document.getElementById("button").style.borderColor = "orange";
 
-        deleteCol[0].style.display = "table_cell";
-        parentElm.previousElementSibling.style.display = delBtn === '' ? 'table-cell' : '';
-        parentElm.previousElementSibling.innerHTML = `<td style="display: table-cell"><button onclick = "delete_row(this)">Delete</button></td>`;
-        editCol[0].style.display = "table_cell";
-        parentElm.style.display = editBtn ==='' ? 'table_cell' : '';
-        parentElm.innerHTML = `<td style="display: table_cell"><button onclick = "edit_row(this)">Edit</button></td>`;
+        delete_col[0].style.display = "table-cell";
+        parent_element.previousElementSibling.style.display = del_btn === '' ? 'table-cell' : '';
+        parent_element.previousElementSibling.innerHTML = `<td style="display: table-cell"><button onclick = "delete_row(this)">Delete</button></td>`;
+        edit_col[0].style.display = "table-cell";
+        parent_element.style.display = edit_btn ==='' ? 'table-cell' : '';
+        parent_element.innerHTML = `<td style="display: table-cell"><button onclick = "edit_row(this)">Edit</button></td>`;
       }
       else if(count_checkbox == 0)
       {
         document.getElementById("button").style.backgroundColor = "gray";
         document.getElementById("button").style.borderColor = "gray"; 
-        deleteCol[0].style.display = "none";
-        parentElm.previousElementSibling.style.display = delBtn === '' ? 'table-cell' : '';
-        editCol[0].style.display = "none";
-        parentElm.style.display = editBtn ==='' ? 'table_cell' : '';
+        delete_col[0].style.display = "none";
+        parent_element.previousElementSibling.style.display = del_btn === '' ? 'table-cell' : '';
+        edit_col[0].style.display = "none";
+        parent_element.style.display = edit_btn ==='' ? 'table-cell' : '';
       }
       if(this.checked) {
         var row = this.parentNode.parentNode.rowIndex;
@@ -165,21 +129,56 @@ function checkboxChange(value) {
       unchecked_row.style.backgroundColor = "white";
       }
     })
-  } // end of checkboxChange()
+  } 
 
-//Delete row function
-function deleteRow(row) {
-  var parent = row.parentNode; 
+
+
+//function to delete the rows using delte button
+
+function delete_row(row) {
+  var prime = row.parentNode; 
   var dropDown = row.parentNode.parentNode.nextElementSibling;
-  parent.parentNode.nextElementSibling.remove(dropDown);//Removing the drop down text
-  parent.parentNode.remove(parent);//Removing the row
+  // drop down txt delete
+  prime.parentNode.nextElementSibling.remove(dropDown);
+  // row delete
+  prime.parentNode.remove(prime);
   count_checkbox--;
   var tableLength = document.querySelectorAll('tbody > tr').length;
   if(count_checkbox == 0)
   {
     document.getElementById("button").style.backgroundColor = "gray";
     document.getElementById("button").style.borderColor = "gray";
-    deleteCol[0].style.display = "none";
-    editCol[0].style.display = "none";
+    delete_col[0].style.display = "none";
+    edit_col[0].style.display = "none";
   }
 }
+
+//Function to edit the row contents
+
+function edit_row(row) {
+  prompt("Editing row contents");
+}
+
+//drop dpwn button \/ click action 
+function imageAction() {
+var img_tag = document.querySelectorAll("img");
+var dropDTA = document.getElementsByClassName("dropDownTextArea");
+  for(i=0; i<img_tag.length; i++)
+  {
+    img_tag[i].onclick = function() {
+      var parent_text_node = this.parentElement;
+      var text_node = parent_text_node.parentElement.nextElementSibling;
+      if(text_node.style.display === "block")
+      {
+        text_node.style.display = "none";
+      }
+      else 
+      {
+        text_node.style.display = "block";
+      }
+    }
+  }
+}
+
+
+
