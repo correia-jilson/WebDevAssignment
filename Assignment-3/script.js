d5d00f7aa6c78b7b02812af8f454204cd0d9a1cd
Jilson Correia <correia.ji@northeastern.edu>
2024-02-01 13:37:54 -0500

    index script edits

--- Assignment-3/script.js
+++ Assignment-3/script.js
@@ -16,3 +16,34 @@ var socialMedia = {
 };
 
 var t = new Title("CONNECT WITH ME!");
+
+// for onload actions 
+window.onload = function() {
+  const col = [];
+  const checbox = document.querySelectorAll('inout[type = checkbox]');
+  for( i=0 ; i < checkbox.length ; i++ ) {
+    checboxChange(i);
+  }
+}
+
+// for add and delete the columns
+const delete_col = document.getElementsByClassName("delete_col");
+const edit_col = document.getElementsByClassName("edit_col");
+
+var count_checkbox = 0;
+
+// for adding new elements 
+function new_element() {
+  var len = table.length;
+  var table = document.querySelectorAll('tbody > tr');
+  var col = document.getElementsById(" myTable ").rows[0].cells.length;
+  const max = 99999;
+  const min = 10000;
+  var row_count = 0;
+  for( i = 1 ; i <= table.length ; i++ ){
+    if( i % 2 != 0 ) {
+      row_count++ ;
+    }
+  }
+  
+}
